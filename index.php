fa272e2b5402db22398730e0589a3265cea42956
fredchen <imfred50215@gmail.com>
2022-07-12 23:11:16 +0800

    以履歷覆蓋

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+    <title>Fred Website</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="shortcut icon" href="assets/favicon.ico" />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+    <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+</head>
+
+<body class="landing">
+
+    <!-- Page Wrapper -->
+    <div id="page-wrapper">
+
+        <!-- Header -->
+        <header id="header" class="alt">
+            <h1><a href="index.php">FredChen</a></h1>
+            <nav id="nav">
+                <ul>
+                    <li class="special">
+                        <a href="#menu" class="menuToggle"><span>Menu</span></a>
+                        <div id="menu">
+                            <ul>
+                                <li><a href="index.php">Home</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <!-- <li><a href="works.html">Works</a></li> -->
+                                <li><a href="https://www.dropbox.com/s/6f4xge4djdhc57f/resume%2Bautobiography.pdf?dl=0" target="_blank" class="icon fa-download">CV</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Banner -->
+        <section id="banner">
+            <div class="inner">
+                <h2>FredChen</h2>
+                <p>Coding
+                    <br /> Music
+                    <br /> Life</p>
+            </div>
+            <a href="#one" class="more scrolly">More</a>
+        </section>
+
+        <!-- One -->
+        <section id="one" class="wrapper style1 special">
+            <div class="inner">
+                <header class="major">
+                    <h2>以成為一個專業的前後端工程師為目標</h2>
+                    <p>Being a professional Front-End and Back-End engineer is my goal.
+                        <br /> Java/HTML/CSS
+                    </p>
+                </header>
+                <ul class="icons major">
+                    <li><span class="icon fa-code major style1"><span class="label">Java</span></span>
+                    </li>
+                    <li><span class="icon fa-html5 major style2"><span class="label">HTML</span></span>
+                    </li>
+                    <li><span class="icon fa-css3 major style3"><span class="label">CSS</span></span>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <!-- Two -->
+        <section id="two" class="wrapper alt style2">
+            <section class="spotlight">
+                <div class="image"><img src="images/pic01.jpg" alt="" /></div>
+                <div class="content">
+                    <h2>目前職位<br/>
+                    Now</h2>
+                    <p>易遊網股份有限公司 產品技術部 後端工程師</p>
+                </div>
+            </section>
+            <section class="spotlight">
+                <div class="image"><img src="images/pic02.jpg" alt="" /></div>
+                <div class="content">
+                    <h2>經歷<br/>
+                      Experience</h2>
+                    <p>2016 易遊網股份有限公司 產品技術部 實習生</p>
+                    <p>2015 大同世界科技股份有限公司 實習生</p>
+                </div>
+            </section>
+            <section class="spotlight">
+                <div class="image"><img src="images/pic03.jpg" alt="" /></div>
+                <div class="content">
+                    <h2>學歷<br />
+								Education</h2>
+                    <p>2012-2016 國立臺北科技大學 資訊工程系<br/>
+                      2009-2012 國立臺南高級工業職業學校 資訊科
+                    </p>
+                </div>
+            </section>
+        </section>
+
+        <!-- Three -->
+        <section id="three" class="wrapper style3 special">
+            <div class="inner">
+                <header class="major">
+                    <h2>興趣</h2>
+                    <p>喜歡多方嘗試<br />
+                        不喜歡一成不變的生活</p>
+                </header>
+                <ul class="features">
+                    <li class="icon fa-code">
+                        <h3>程式</h3>
+                        <p>HTML、CSS<br/>
+                           JAVA<br/>
+                           PHP<br/>
+                           C、C++、C#<br/>
+                           Android App</p>
+                    </li>
+                    <li class="icon fa-headphones">
+                        <h3>音樂</h3>
+                        <p>國小 樂隊校隊<br/>
+                          國小國中 合唱校隊</p>
+                    </li>
+                    <li class="icon fa-plane">
+                        <h3>旅遊</h3>
+                        <p>2010 日本 東京、千葉<br/>
+                           2016 日本 大阪、京都<br/>
+                           2017 香港 中環、上環</p>
+                    </li>
+                    <li class="icon fa-trophy">
+                        <h3>證照</h3>
+                        <p>2015 TOEIC 640<br/>
+                        2010 電腦軟體應用技術士乙級<br/>
+                        2010 電腦軟體設計技術士丙級<br/>
+                        2009 電腦軟體應用技術士丙級</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+
+        <!-- CTA -->
+        <section id="cta" class="wrapper style4">
+            <div class="inner">
+                <header>
+                    <h2>座右銘</h2>
+                    <p>今天要比昨天更好，這樣今天才有價值</p>
+                </header>
+                <ul class="actions vertical">
+                    <li><a href="#" class="button fit">回頁首</a></li>
+                </ul>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer id="footer">
+            <ul class="icons">
+                <li><a href="https://www.facebook.com/Chen.PinCheng" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+                <li><a href="https://www.instagram.com/fred50215/" class="icon fa-instagram" target="_blank"><span class="label">Instagram</span></a></li>
+                <li><a href="mailto:imfred50215@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+            </ul>
+            <ul class="copyright">
+                <li>&copy; FredChen</li>
+                <li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            </ul>
+        </footer>
+
+    </div>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
